9dc7070a65bbccaf8edb4d82c93194e89c3702df
billykwooten <billykwooten@gmail.com>
2019-04-05 08:11:29 -0400

    Adding variables for hour/minute

--- main.go
+++ main.go
@@ -12,19 +12,19 @@ var (
 	app   = kingpin.New("darksky_exporter", "DarkSky Exporter for DarkSky Weather API").Author("Billy Wooten")
 	csvloc = app.Flag("csv", "File path of the CSV file").Envar("CSVLOC").Required().String()
 	slackhook = app.Flag("slackhook", "Webhook URL for Slacks").Envar("SLACKHOOK").Required().String()
+	csvhour = app.Flag("csvhour", "Set the hour in which the script will run").Envar("CSVHOUR").Default("8").Int()
+	csvminute = app.Flag("csvminute", "Set the minute in which the script will run").Envar("CSVMINUTE").Default("0").Int()
 	log = logger.New("CSVMonitor")
 
 )
 
 func initEight() {
-	var hour int
-	hour = 8
 	t := time.Now()
-	n := time.Date(t.Year(), t.Month(), t.Day(), hour, 0, 0, 0, t.Location())
+	n := time.Date(t.Year(), t.Month(), t.Day(), *csvhour, *csvminute, 0, 0, t.Location())
 	d := n.Sub(t)
 	if d < 0 {
 		log.Info("Waiting 24 hours to run again")
-		log.Info("Will run again at hour %d", hour)
+		log.Info("Will run again at hour %d", *csvhour, ":", *csvminute)
 		n = n.Add(24 * time.Hour)
 		d = n.Sub(t)
 	}
